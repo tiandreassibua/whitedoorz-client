ef06cf7ef5fa0cea6372a59ed94c28f8724ed1ba
Andreas Sibua <andreas.sibua@ti.ukdw.ac.id>
2023-12-08 09:11:30 +0700

    pindah repository

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { FaBed } from "react-icons/fa";
+
+function App() {
+    return (
+        <h1 className="text-3xl text-slate-700 font-semibold">
+            Hotel{" "}
+            <span>
+                <FaBed />
+            </span>
+        </h1>
+    );
+}
+
+export default App;
