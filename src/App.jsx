c82a4b80f632e87743e77ae5740b4feeea1a3134
Elizabeth Queeny <93310086+elzbthqn@users.noreply.github.com>
2023-12-28 18:28:38 +0900

    add pages succes and error payment

--- src/App.jsx
+++ src/App.jsx
@@ -15,33 +15,46 @@ import Profile from "./pages/Profile";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Rating from "./pages/Rating";
+import Success from "./pages/Success";
+import Error from "./pages/Error";
 
 function App() {
-  return (
-    <div className="font-poppins">
-      <BrowserRouter>
-        <ToastContainer />
-        <Routes>
-          <Route path="*" element={<NotFound />} />
-          <Route path="/" element={<DefaultLayout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/:slug" element={<PropertyDetailPage />} />
-            <Route path="/order" element={<Order />} />
-            <Route path="/history" element={<History />} />
-            <Route path="/rating/:transactionId" element={<Rating />} />
-            <Route path="/S&K" element={<SAK />} />
-            <Route path="/wishlists" element={<Wishlist />} />
-            <Route path="/search/:keyword" element={<Search />} />
-            <Route path="/profile" element={<Profile />} />
-          </Route>
-          <Route path="/" element={<AuthLayout />}>
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/login" element={<LoginPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="font-poppins">
+            <BrowserRouter>
+                <ToastContainer />
+                <Routes>
+                    <Route path="*" element={<NotFound />} />
+                    <Route path="/" element={<DefaultLayout />}>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/:slug" element={<PropertyDetailPage />} />
+                        <Route
+                            path="/success"
+                            element={<Success />}
+                        />
+                        <Route
+                            path="/error"
+                            element={<Error />}
+                        />
+                        <Route path="/order" element={<Order />} />
+                        <Route path="/history" element={<History />} />
+                        <Route
+                            path="/rating/:transactionId"
+                            element={<Rating />}
+                        />
+                        <Route path="/S&K" element={<SAK />} />
+                        <Route path="/wishlists" element={<Wishlist />} />
+                        <Route path="/search/:keyword" element={<Search />} />
+                        <Route path="/profile" element={<Profile />} />
+                    </Route>
+                    <Route path="/" element={<AuthLayout />}>
+                        <Route path="/register" element={<RegisterPage />} />
+                        <Route path="/login" element={<LoginPage />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/Success.jsx
@@ -0,0 +1,44 @@
+import {
+    Card,
+    CardBody,
+    CardFooter,
+    Typography,
+} from "@material-tailwind/react";
+
+import { BsCheckCircle } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
+const Success = () => {
+    return (
+        <div className="flex justify-center items-center">
+            <div className="my-10">
+                <Card className="my-10 shadow-none">
+                    <CardBody>
+                        <BsCheckCircle className="text-green-500 text-[200px] text-center w-full my-10" />
+                        <Typography
+                            variant="h2"
+                            color="blue-gray"
+                            className="mb-2 text-center"
+                        >
+                            Pembayaran Berhasil
+                        </Typography>
+                        <Typography className="text-center text-gray-500">
+                            Silahkan cek halaman history untuk melihat status
+                            pesanan anda.
+                        </Typography>
+                    </CardBody>
+                    <CardFooter className="pt-5 flex justify-center">
+                        <Link
+                            className="py-3 px-6 rounded-md font-semibold bg-gray-900 hover:shadow-lg active:bg-gray-800 text-white transition-all duration-100"
+                            to={"/history"}
+                        >
+                            History
+                        </Link>
+                    </CardFooter>
+                </Card>
+            </div>
+        </div>
+    );
+};
+
+export default Success;
